8899f6a92780ee105b4f54d1e1ec7249f8516e74
Amkam <amaduskamara@gmail.com>
2020-07-21 15:42:14 +0000

    SYM Blog app initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')  
+  <h1 class="font-weight-light">{{ $title }}</h1>
+  <p>This is the About page of the application. Talk about what :))))) git testing.</p>
+@endsection
